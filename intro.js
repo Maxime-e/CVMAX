dcfdc858d861ad1eace4127e21bf9352e90f4fe4
Maxime-e <89695679+Maxime-e@users.noreply.github.com>
2021-10-19 17:34:12 +0200

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+<a href="http://memo-web.fr" title="Ouverture d'une nouvelle fenêtre popup" 
+     onclick="javascript:window.open(this.href,'nom_Popup',
+                           'height=800 , width=600 , 
+                            location=no , 
+                            resizable=yes ,
+                            scrollbars=yes');return false;"> Exemple d'ouverture d'une fenêtre en popup
+</a>
