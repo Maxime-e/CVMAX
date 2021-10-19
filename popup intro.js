6c836bbfa790a846b023fd80f12592891f98ec1e
Maxime-e <89695679+Maxime-e@users.noreply.github.com>
2021-10-19 16:09:07 +0200

    Add files via upload

---
+++ popup intro.js
@@ -0,0 +1,7 @@
+<a href="https://github.com/Maxime-e/CVMAX/blob/main/index.html" title="intro" 
+     onclick="javascript:window.open(this.href,'nom_Popup',
+                           'height=400 , width=400 , 
+                            location=no , 
+                            resizable=yes ,
+                            scrollbars=yes');return false;"> Exemple d'ouverture d'une fenêtre en popup
+</a>    
